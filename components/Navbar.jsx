0631bbbf64ef11b2bdb9a3c7dfa4fb0792c952d0
Robin <mahamuniaditya07@gmail.com>
2024-03-29 00:59:12 +0530

    resolved tailwind

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import Web3 from "web3";
+import { useRouter } from "next/router";
 
 const Nav = () => {
   const connectMetamask = async () => {
@@ -22,17 +23,17 @@ const Nav = () => {
   };
 
   return (
-    <nav className="flex-between w-full mb-16 pt-3 gap-10">
+    <nav className="flex-between w-full mb-16 pt-3 gap-10 pl-16 pr-16">
       <Link href="/" className="flex gap-2 flex-center">
         <p className="logo_text">Noktua</p>
       </Link>
-      <div className="flex-between ml-4 mr-4">
+      <div className="flex-between ml-8 mr-4 gap-4">
         <p className="logo_text">Home</p>
         <p className="logo_text">Drop</p>
         <p className="logo_text">Stats</p>
         <p className="logo_text">Create</p>
       </div>
-      <input type="text" placeholder="Search..." required className="peer" />
+      <input type="text" placeholder="Search..." required className="search_input30 peer" />
       <button className="black_btn" onClick={connectMetamask}>
         Login
       </button>
